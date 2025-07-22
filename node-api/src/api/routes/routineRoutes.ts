3271f96b5f11e0ef6499d548e3e91f6a4537352b
Oluwapelumi Kehinde <63884321+SirPelzy@users.noreply.github.com>
2025-07-22 16:10:45 +0100

    Update routineRoutes.ts

--- node-api/src/api/routes/routineRoutes.ts
+++ node-api/src/api/routes/routineRoutes.ts
@@ -1,1 +1,31 @@
+import { Router } from 'express';
+import { protect } from '../middlewares/authMiddleware';
+import {
+    getUserRoutines,
+    createRoutine,
+    updateRoutine,
+    deleteRoutine,
+} from '../controllers/routineController';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validationMiddleware';
 
+const router = Router();
+
+// All routine routes are protected
+router.use(protect);
+
+router.route('/')
+    .get(getUserRoutines)
+    .post(
+        [
+            body('name').notEmpty().withMessage('Routine name is required'),
+        ],
+        validateRequest,
+        createRoutine
+    );
+
+router.route('/:id')
+    .put(updateRoutine)
+    .delete(deleteRoutine);
+
+export default router;
