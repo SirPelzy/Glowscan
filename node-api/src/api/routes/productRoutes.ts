c6db4c57206fb686992dedeb57ec18969b8ddad3
Oluwapelumi Kehinde <63884321+SirPelzy@users.noreply.github.com>
2025-07-23 17:39:45 +0100

    Update productRoutes.ts

--- node-api/src/api/routes/productRoutes.ts
+++ node-api/src/api/routes/productRoutes.ts
@@ -1,1 +1,12 @@
+import { Router } from 'express';
+import { getProductRecommendations } from '../controllers/productController';
+import { protect } from '../middlewares/authMiddleware';
 
+const router = Router();
+
+// All product routes should be protected
+router.use(protect);
+
+router.get('/recommendations', getProductRecommendations);
+
+export default router;
